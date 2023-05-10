c3a2b6ef003dd704a34df0474a09a6ae14c1aa93
Greviansonula <greviansonula@gmail.com>
2023-05-10 22:40:12 +0300

    update

--- src/data_load.py
+++ src/data_load.py
@@ -6,38 +6,61 @@ from PIL import Image
 import xml.etree.ElementTree as ET
 
 class ChangeDetectionDataset(Dataset):
-    def __init__(self, data_dir):
-        self.data_dir = data_dir
-        self.image_dir = os.path.join(data_dir, "images")
-        self.label_dir = os.path.join(data_dir, "labels")
-        self.xml_dir = os.path.join(data_dir, "xml")
-
-        self.image_filenames = sorted(os.listdir(self.image_dir))
-        self.label_filenames = sorted(os.listdir(self.label_dir))
-        self.xml_filenames = sorted(os.listdir(self.xml_dir))
-
-        self.transform = ToTensor()
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_filenames = sorted(os.listdir(os.path.join(root_dir, 'images')))
+        self.label_filenames = sorted(os.listdir(os.path.join(root_dir, 'labels')))
 
     def __len__(self):
         return len(self.image_filenames)
 
-    def __getitem__(self, index):
-        image_filename = self.image_filenames[index]
-        label_filename = self.label_filenames[index]
-        xml_filename = self.xml_filenames[index]
+    def __getitem__(self, idx):
+        image_path = os.path.join(self.root_dir, 'images', self.image_filenames[idx])
+        label_path = os.path.join(self.root_dir, 'labels', self.label_filenames[idx])
 
-        image_path = os.path.join(self.image_dir, image_filename)
-        label_path = os.path.join(self.label_dir, label_filename)
-        xml_path = os.path.join(self.xml_dir, xml_filename)
+        image = Image.open(image_path).convert('RGB')
+        label = Image.open(label_path).convert('L')  # Convert label to grayscale
 
-        image = Image.open(image_path).convert("RGB")
-        label = Image.open(label_path).convert("L")
-        xml_tree = ET.parse(xml_path)
+        if self.transform:
+            image = self.transform(image)
+            label = self.transform(label)
 
-        sample = {
-            "image": self.transform(image),
-            "label": self.transform(label),
-            "xml_tree": xml_tree
-        }
+        return image, label
 
-        return sample
+# class ChangeDetectionDataset(Dataset):
+#     def __init__(self, data_dir):
+#         self.data_dir = data_dir
+#         self.image_dir = os.path.join(data_dir, "images")
+#         self.label_dir = os.path.join(data_dir, "labels")
+#         self.xml_dir = os.path.join(data_dir, "xml")
+
+#         self.image_filenames = sorted(os.listdir(self.image_dir))
+#         self.label_filenames = sorted(os.listdir(self.label_dir))
+#         self.xml_filenames = sorted(os.listdir(self.xml_dir))
+
+#         self.transform = ToTensor()
+
+#     def __len__(self):
+#         return len(self.image_filenames)
+
+#     def __getitem__(self, index):
+#         image_filename = self.image_filenames[index]
+#         label_filename = self.label_filenames[index]
+#         xml_filename = self.xml_filenames[index]
+
+#         image_path = os.path.join(self.image_dir, image_filename)
+#         label_path = os.path.join(self.label_dir, label_filename)
+#         xml_path = os.path.join(self.xml_dir, xml_filename)
+
+#         image = Image.open(image_path).convert("RGB")
+#         label = Image.open(label_path).convert("L")
+#         xml_tree = ET.parse(xml_path)
+
+#         sample = {
+#             "image": self.transform(image),
+#             "label": self.transform(label),
+#             "xml_tree": xml_tree
+#         }
+
+#         return sample
---
+++ src/train.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+from yolov5.models.experimental import attempt_load
+from yolov5.utils.torch_utils import select_device
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+from unet import UNet
+from data_load import ChangeDetectionDataset
+
+
+
+device = select_device('')  # Use an empty string to automatically select a device
+yolo_model = attempt_load('weights/yolov5s.pt', map_location=device)  # Adjust the path to your pre-trained model
+yolo_model.eval()
+
+in_ch = 3  # Number of input channels (e.g., RGB images)
+out_ch = 1  # Number of output channels (e.g., binary mask)
+
+unet_model = UNet(in_ch, out_ch)  # Adjust the input and output channels as per your requirements
+
+loss_fn = nn.CrossEntropyLoss()  # Example loss function, adjust as per your requirements
+
+optimizer = optim.Adam(unet_model.parameters(), lr=0.001)  # Example optimizer, adjust as per your requirements
+
+batch_size = 16
+num_epochs = 10
+
+transform = ToTensor()  # Adjust the transformation based on your data requirements
+train_dataset = ChangeDetectionDataset('data_train', transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+for epoch in range(num_epochs):
+    for batch in train_loader:
+        images, labels = batch  # Adjust this based on your data format
+
+        # Extract features using YOLOv5
+        with torch.no_grad():
+            yolo_outputs = yolo_model(images)
+
+        # Pass the extracted features through UNet
+        unet_outputs = unet_model(yolo_outputs)
+
+        # Compute the loss
+        loss = loss_fn(unet_outputs, labels)
+
+        # Backward pass and optimization
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+test_dataset = ChangeDetectionDataset('data_detect', transform=transform)
+test_loader = DataLoader(test_dataset, batch_size=1)
+
+unet_model.eval()  # Set the UNet model to evaluation mode
+for batch in test_loader:
+    images, labels = batch  # Adjust this based on your data format
+
+    with torch.no_grad():
+        # Extract features using YOLOv5
+        yolo_outputs = yolo_model(images)
+
+        # Pass the extracted features through UNet
+        unet_outputs = unet_model(yolo_outputs)
+
+        # Process the outputs as per your requirements
+
+        # If segmentation functionality is enabled, apply the segmentation logic
+
+        # Obtain the localization and segmentation results
+
+        # Perform any necessary post-processing or evaluation
+
+
